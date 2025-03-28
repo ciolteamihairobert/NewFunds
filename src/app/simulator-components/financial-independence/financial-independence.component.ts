b12c72f17a5a4cd4abd2b1393dd9a34ac98ff4a0
Andrei Mihai <andrei.mihai2@student.upt.ro>
2025-03-28 12:00:21 +0200

    Fixed the component

--- src/app/simulator-components/financial-independence/financial-independence.component.ts
+++ src/app/simulator-components/financial-independence/financial-independence.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../reusable-components/header/header.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { FormDataService } from '../../reusable-components/services/form-data.service';
 import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
@@ -18,8 +17,24 @@ import { MatGridListModule } from '@angular/material/grid-list';
   styleUrl: '../simulatorStyles.css'
 })
 export class FinancialIndependenceComponent {
- public clientInfoForm: FormGroup<any> = new FormGroup<any>({});
- income: number = 0;
- rentalYield: Number = 0;
-
+ public income: number = 0;
+ public rentalYield: Number = 0;
+ public concept1 = {
+  title: 'Varianta 1 de folosire a Conceptului',
+  description: 'Aveti posiblitatea ca sumele primite la finalul contractului sa le utilizati pentru a va putea genera in continuare profituri si venituri pasive, astfel va veti putea bucura mai mult de aspectele ce conteaza pentru dumneavoastra. In plus, suma din cont poate fi lasata mostenire.',
+  contentItems: [
+    { text: 'Astfel, la varsta de x ani valoarea contului dumneavoastra va fi de', value: 'Z EURO' },
+    { text: 'Veniturile pasive totale pe perioada de x ani, sunt de', value: 'Z EURO' },
+    { text: 'Venitul pasiv lunar primit in aceasta perioada este de', value: 'Z EURO' },
+    { text: 'Beneficiul total, calculat ca suma dintre veniturile pasive primite de catre dumneavoastra si mostenirea lasata familiei, este de', value: 'Z EURO' },
+  ],
+};
+ public concept2 = {
+  title: 'Varianta 2 de folosire a Conceptului',
+  description: 'O alta posibilitate de utilizare a acestui concept este aceea de a genera venituri pasive atat din randamente cat si din sumele din cont. Astfel, avantajul este ca suma pe care o veti primi lunar va fi mai mare, dar livrandu-se catre dumneavoastra toate sumele din cont, la final nu vor mai ramane bani pentru mostenire.',
+  contentItems: [
+    { text: 'Veniturile pasive totale pe perioada de x ani, sunt de', value: 'Z EURO' },
+    { text: 'Venitul pasiv lunar primit in aceasta perioada este de', value: 'Z EURO' },
+  ],
+};
 }
