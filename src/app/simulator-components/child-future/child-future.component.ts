402be7b7c53626810df244bf5c4ebeccea8bbb12
Andrei Mihai <andrei.mihai2@student.upt.ro>
2025-03-28 18:47:33 +0200

    Added the table layout in 'Child future'

--- src/app/simulator-components/child-future/child-future.component.ts
+++ src/app/simulator-components/child-future/child-future.component.ts
@@ -5,6 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-child-future',
@@ -12,10 +13,18 @@ import { MatGridListModule } from '@angular/material/grid-list';
   imports: [HeaderComponent,  MatFormFieldModule,
     MatInputModule,FormsModule,  CommonModule, 
     ReactiveFormsModule, MatGridListModule,
-    ],
+    MatTableModule,
+  ],
   templateUrl: './child-future.component.html',
   styleUrl: '../simulatorStyles.css'
 })
 export class ChildFutureComponent {
-
+ public  displayedColumns: string[] = ['age', 'balance', 'withdrawals'];
+ public dataSource = ELEMENT_DATA;  
 }
+
+const ELEMENT_DATA = [
+  { age: '5 ani', balance: '10,000 EURO', withdrawals: '500 EURO' },
+  { age: '10 ani', balance: '15,000 EURO', withdrawals: '700 EURO' },
+  { age: '15 ani', balance: '20,000 EURO', withdrawals: '1,000 EURO' },
+];
